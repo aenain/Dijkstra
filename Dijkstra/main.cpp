84bbcf8ebd667a672769dc3dcbeb9f9f0432a68a
Artur Hebda <artur@adtaily.com>
2011-05-28 12:17:05 +0200

    Initial Commit

---
+++ Dijkstra/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Dijkstra
+//
+//  Created by Artur Hebda on 28/05/2011.
+//  Copyright 2011 AdTaily. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
