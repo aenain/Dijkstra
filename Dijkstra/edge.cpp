68ad9a60728db5c06ce57cb3a4fb530423950833
Artur Hebda <artur@adtaily.com>
2011-05-31 09:22:22 +0200

    Fuck yeah! działają mi template'y! Dziękuję, Panie Doktorze :)

--- Dijkstra/edge.cpp
+++ Dijkstra/edge.cpp
@@ -8,22 +8,4 @@
 #include <iostream>
 #include "edge.h"
 
-template <class T>
-Distance Edge<T>::compute_weight(T begin, T end) {
-    return sqrt( pow(end.latitude - begin.latitude, 2) + pow(end.longitude - begin.latitude, 2) );
-}
-
-template <class T>
-Distance Edge<T>::weight() {
-    return _weight;
-}
-
-template <class T>
-bool Edge<T>::operator<(const Edge<T> other) {
-    return weight() > other.weight();
-}
-
-template <class T>
-bool operator<(Edge<T> a, Edge<T> b) {
-    return a.weight() > b.weight();
-}
+// Definition of template has to be in header file, because compiler requires whole template to build instances for given class types.
--- Dijkstra/edge.h
+++ Dijkstra/edge.h
@@ -27,8 +27,24 @@ private:
     Distance compute_weight(T const begin, T const end);
 };
 
+template <class T>
+Distance Edge<T>::compute_weight(T begin, T end) {
+    return sqrt( pow(end.latitude - begin.latitude, 2) + pow(end.longitude - begin.latitude, 2) );
+}
 
 template <class T>
-bool operator<(Edge<T> a, Edge<T> b);
+Distance Edge<T>::weight() {
+    return _weight;
+}
+
+template <class T>
+bool Edge<T>::operator<(const Edge<T> other) {
+    return weight() > other.weight();
+}
+
+template <class T>
+bool operator<(Edge<T> a, Edge<T> b) {
+    return a.weight() > b.weight();
+}
 
 #endif
